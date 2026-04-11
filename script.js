4ce264bf26d821e10cd01315ca1ddc9ccd388d7b
Flaviu Maxim <flaviomaxim@gmail.com>
2026-04-12 01:16:10 +0200

    commmit 1

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+console.log("DreamBouw cargado 🚀");
 
+// BOTÓN INTERACTIVO
+function saludar() {
+    alert("Bienvenido a DreamBouw 🔥");
+}
+
+// CAMBIO DE COLOR DINÁMICO
+let titulo = document.querySelector("h1");
+
+titulo.addEventListener("click", () => {
+    titulo.style.color = "#ff4d4d";
+});
+
+// MENSAJE AL CARGAR
+window.onload = () => {
+    console.log("Página lista");
+};
